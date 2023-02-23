4ea7b35e533666eabfd45061504a0b6ca35bb96f
demjanp <peter.demjan@gmail.com>
2023-02-23 13:14:07 +0100

    updated About section; updated CSS stylesheet

--- src/arch14cz_frontend/about.php
+++ src/arch14cz_frontend/about.php
@@ -1,13 +1,11 @@
+<?php
+require_once("Parsedown.php");
+$parsedown = new Parsedown();
+$text = file_get_contents("about.md");
+?>
 <div class="column">
-	<h2>About</h2>
-	<div class="text">
-		<p>Lorem ipsum dolor sit amet, <span class="emphasis">consectetur adipiscing elit</span>, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. A iaculis at erat pellentesque adipiscing commodo elit at. Sapien eget mi proin sed libero. Lobortis feugiat vivamus at augue eget arcu dictum varius duis. Sit amet mattis vulputate enim <a href = "#">nulla aliquet</a> porttitor. Dignissim convallis aenean et tortor at risus viverra. Ac tincidunt vitae semper quis lectus. Commodo sed egestas egestas fringilla phasellus. Magna ac placerat vestibulum lectus mauris ultrices. Augue interdum velit euismod in pellentesque massa placerat duis.</p>
-	</div>
-	<div class="image">
-		<img src="static/arch14cz_rel_dating_order.png">
-		<p>Commodo sed egestas egestas fringilla phasellus.</p>
-	</div>
-	<div class="text">
-		<p>Lacus luctus accumsan tortor posuere. Vestibulum sed arcu non odio euismod lacinia at quis risus. Malesuada nunc vel risus commodo viverra maecenas accumsan lacus vel. Vestibulum lorem sed risus ultricies tristique. In aliquam sem fringilla ut morbi tincidunt augue interdum. Facilisis leo vel fringilla est. Tristique senectus et netus et <span class="emphasis">malesuada fames</span> ac. In tellus integer feugiat scelerisque varius morbi enim nunc. Aliquam purus sit amet luctus. Egestas integer eget aliquet nibh. Amet tellus cras adipiscing enim eu turpis egestas. Pellentesque habitant morbi tristique senectus et. Tellus pellentesque eu tincidunt tortor aliquam nulla. Gravida quis blandit turpis cursus in hac habitasse. Tellus orci ac auctor augue.</p>
-	</div>
+<?php
+echo $parsedown->text($text);
+//echo $text;
+?>
 </div>
